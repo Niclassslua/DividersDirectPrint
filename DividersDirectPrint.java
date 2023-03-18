86c6d43e70da1cc0b820be1a3174442b74be5e5c
Niclassslua <78554432+Niclassslua@users.noreply.github.com>
2023-03-18 04:17:14 +0100

    Update DividersDirectPrint.java

--- DividersDirectPrint.java
+++ DividersDirectPrint.java
@@ -1,26 +1,26 @@
-package uebung02;
-
-import static pr.MakeItSimple.print;
-import static pr.MakeItSimple.println;
-import static pr.MakeItSimple.readInt;
+package DividersDirectPrint;
 
 public class DividersDirectPrint {
 	public static void main(String[] args) {
-		println("Bitte geben Sie eine ganze Zahl > 0 ein: ");
+		
+		System.out.println("Bitte geben Sie eine ganze Zahl > 0 ein: ");
+		
 		int number = readInt();
+		
 		if (number < 0) {
-			println("Eingabe ungueltig!");
+			System.out.println("Eingabe ungueltig!");
 		} else {
 			int counter = number;
 			while (counter > 0) {
 				if (number % counter == 0) {
-					print(counter);
+					System.out.print(counter);
 					if (counter != 1) {
-						print(", ");
+						System.out.print(", ");
 					}
 				}
 				counter--;
 			}
 		}
+		
 	}
 }
